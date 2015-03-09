8caf26ad158049a0732b3b73836327ad000081cc
Spencer Tung <spencer.zh.tung@gmail.com>
2015-03-08 17:53:48 -0700

    Removed unused variable in lines_of_code.sh

--- instrument/lines_of_codes.sh
+++ instrument/lines_of_codes.sh
@@ -2,7 +2,7 @@
 
 d=../src
 
-for f in $(cd $d/$v1; find test -name '*\.java')
+for f in $(cd $d; find test -name '*\.java')
 do
   java -cp locc-4.2.jar csdl.locc.sys.LOCTotal -sizetype javaline -infiles $d/$f
 done
